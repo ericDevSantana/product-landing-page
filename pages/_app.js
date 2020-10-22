20d3a93a72543864ca91cc154dade27ca98ef998
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-10-21 19:18:45 -0700

    Navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import { Navbar } from 'react-bootstrap'
 import '../styles/globals.css'
+import NavigationBar from '../pages/Partials/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavigationBar/>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
