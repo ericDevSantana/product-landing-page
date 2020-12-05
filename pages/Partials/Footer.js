3e5e24e165d92fb5b52de4002bcdbf2bae211ace
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-12-04 23:24:27 -0800

    adding contact form

--- pages/Partials/Footer.js
+++ pages/Partials/Footer.js
@@ -4,14 +4,7 @@ export default function Footer() {
     return (
         <>
         <footer className={styles.footer}>
-            {/* <a
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-            Powered by{' '}
-            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-            </a> */}
+            All rights reserved. 2020-2021
         </footer>
         </>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Carousel } from 'react-bootstrap'
+import { Carousel, Form, Button } from 'react-bootstrap'
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
@@ -138,16 +138,29 @@ export default function Home() {
 
       </section>
 
-      {/* -------------------------------ABOUT SECTION BELOW-------------------------------*/}
-
-      <section>
-
-      </section>
-
       {/* -------------------------------CONTACT SECTION BELOW-------------------------------*/}
 
       <section>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
       </section>
 
     </div>
